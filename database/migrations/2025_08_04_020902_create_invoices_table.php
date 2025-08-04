2ff6e2e6aac3972e97bcb034a1c6b46da431f56c
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2025-08-04 10:21:20 +0800

    invoices

---
+++ database/migrations/2025_08_04_020902_create_invoices_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('customer_name');
+            $table->string('customer_email')->nullable();
+            $table->tinyInteger('status')->default(0)->comment('0: Unpaid, 1: Paid, 2: Overdue');
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('restrict')->onUpdate('cascade');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
