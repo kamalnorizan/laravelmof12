ae82545d306e7ee09318e6a869757edd48d20027
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2025-08-04 11:35:42 +0800

    route model binding

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Invoice;
 use Illuminate\Http\Request;
 
 class InvoiceController extends Controller
 {
-    //
+    
+
+    public function show(Invoice $invoice) {
+        if (!$invoice) {
+            return response()->json(['message' => 'Invoice not found'], 404);
+        }
+        return response()->json($invoice);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InvoiceController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/invoices/{invoice}', [InvoiceController::class, 'show'])
+    ->name('invoices.show');
