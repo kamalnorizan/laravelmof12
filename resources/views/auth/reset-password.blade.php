279c878c6c85265b74db5d0b82b24808489e5131
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2025-08-05 23:34:26 +0800

    reset password

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,39 +1,94 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('password.store') }}">
-        @csrf
+@extends('layouts.guest')
 
-        <!-- Password Reset Token -->
-        <input type="hidden" name="token" value="{{ $request->route('token') }}">
+@section('title','Reset Password - Pages | Materio - Bootstrap Material Design Admin Template')
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+@section('content')
+<div class="card p-7">
+        <!-- Logo -->
+        <div class="app-brand justify-content-center mt-5">
+            <a href="index.html" class="app-brand-link gap-3">
+                <span class="app-brand-logo demo">
+                    <span style="color: #9055fd">
+                        <svg width="30" height="24" viewBox="0 0 250 196" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd"
+                                d="M12.3002 1.25469L56.655 28.6432C59.0349 30.1128 60.4839 32.711 60.4839 35.5089V160.63C60.4839 163.468 58.9941 166.097 56.5603 167.553L12.2055 194.107C8.3836 196.395 3.43136 195.15 1.14435 191.327C0.395485 190.075 0 188.643 0 187.184V8.12039C0 3.66447 3.61061 0.0522461 8.06452 0.0522461C9.56056 0.0522461 11.0271 0.468577 12.3002 1.25469Z"
+                                fill="currentColor" />
+                            <path opacity="0.077704" fill-rule="evenodd" clip-rule="evenodd"
+                                d="M0 65.2656L60.4839 99.9629V133.979L0 65.2656Z" fill="black" />
+                            <path opacity="0.077704" fill-rule="evenodd" clip-rule="evenodd"
+                                d="M0 65.2656L60.4839 99.0795V119.859L0 65.2656Z" fill="black" />
+                            <path fill-rule="evenodd" clip-rule="evenodd"
+                                d="M237.71 1.22393L193.355 28.5207C190.97 29.9889 189.516 32.5905 189.516 35.3927V160.631C189.516 163.469 191.006 166.098 193.44 167.555L237.794 194.108C241.616 196.396 246.569 195.151 248.856 191.328C249.605 190.076 250 188.644 250 187.185V8.09597C250 3.64006 246.389 0.027832 241.935 0.027832C240.444 0.027832 238.981 0.441882 237.71 1.22393Z"
+                                fill="currentColor" />
+                            <path opacity="0.077704" fill-rule="evenodd" clip-rule="evenodd"
+                                d="M250 65.2656L189.516 99.8897V135.006L250 65.2656Z" fill="black" />
+                            <path opacity="0.077704" fill-rule="evenodd" clip-rule="evenodd"
+                                d="M250 65.2656L189.516 99.0497V120.886L250 65.2656Z" fill="black" />
+                            <path fill-rule="evenodd" clip-rule="evenodd"
+                                d="M12.2787 1.18923L125 70.3075V136.87L0 65.2465V8.06814C0 3.61223 3.61061 0 8.06452 0C9.552 0 11.0105 0.411583 12.2787 1.18923Z"
+                                fill="currentColor" />
+                            <path fill-rule="evenodd" clip-rule="evenodd"
+                                d="M12.2787 1.18923L125 70.3075V136.87L0 65.2465V8.06814C0 3.61223 3.61061 0 8.06452 0C9.552 0 11.0105 0.411583 12.2787 1.18923Z"
+                                fill="white" fill-opacity="0.15" />
+                            <path fill-rule="evenodd" clip-rule="evenodd"
+                                d="M237.721 1.18923L125 70.3075V136.87L250 65.2465V8.06814C250 3.61223 246.389 0 241.935 0C240.448 0 238.99 0.411583 237.721 1.18923Z"
+                                fill="currentColor" />
+                            <path fill-rule="evenodd" clip-rule="evenodd"
+                                d="M237.721 1.18923L125 70.3075V136.87L250 65.2465V8.06814C250 3.61223 246.389 0 241.935 0C240.448 0 238.99 0.411583 237.721 1.18923Z"
+                                fill="white" fill-opacity="0.3" />
+                        </svg>
+                    </span>
+                </span>
+                <span class="app-brand-text demo text-heading fw-semibold">Materio</span>
+            </a>
         </div>
+        <!-- /Logo -->
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        <div class="card-body mt-1">
+            <h4 class="mb-1">Reset Password 🚀</h4>
+            <p class="mb-5">Insert your new password below.</p>
+
+            <form id="formAuthentication" class="mb-5" action="{{ route('password.update') }}" method="post">
+                @csrf
+                <input type="hidden" name="token" value="{{ $request->route('token') }}">
+                <div class="form-floating form-floating-outline mb-5">
+                    <input type="text" class="form-control" id="email" name="email"
+                        placeholder="Enter your email" value="{{ old('email') }}" autofocus />
+                    <label for="email">Email</label>
+                    <small class="text-danger">{{ $errors->first('email') }}</small>
+                </div>
+                <div class="mb-5">
+                    <div class="form-password-toggle">
+                        <div class="input-group input-group-merge">
+                            <div class="form-floating form-floating-outline">
+                                <input type="password" id="password" class="form-control" name="password"
+                                    placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                                    aria-describedby="password" />
+                                <label for="password">Password</label>
+                            </div>
+                            <span class="input-group-text cursor-pointer"><i class="ri-eye-off-line ri-20px"></i></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="mb-5">
+                    <div class="form-password-toggle">
+                        <div class="input-group input-group-merge">
+                            <div class="form-floating form-floating-outline">
+                                <input type="password" id="password_confirmation" class="form-control" name="password_confirmation"
+                                    placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                                    aria-describedby="password" />
+                                <label for="password_confirmation">Reconfirm Password</label>
+                                <small class="text-danger">{{ $errors->first('password_confirmation') }}</small>
+                            </div>
+                            <span class="input-group-text cursor-pointer"><i class="ri-eye-off-line ri-20px"></i></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="mb-5">
+                    <button class="btn btn-primary d-grid w-100" type="submit">Reset Password</button>
+                </div>
+            </form>
         </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Reset Password') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    </div>
+@endsection
