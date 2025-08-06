b36970de852f12dae409b68db9bb7cea1323d800
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2025-08-07 05:29:29 +0800

    buttons

--- resources/js/users.js
+++ resources/js/users.js
@@ -1,6 +1,6 @@
-import JSzip from 'jszip';
+import JSZip from 'jszip';
 import pdfMake from 'pdfmake';
-
+import pdfFonts from 'pdfmake/build/vfs_fonts';
 import DataTable from 'datatables.net-bs5';
 import Buttons from 'datatables.net-buttons-bs5';
 import FixedColumns from 'datatables.net-fixedcolumns-bs5';
@@ -8,8 +8,15 @@ import FixedHeader from 'datatables.net-fixedheader-bs5';
 import Responsive from 'datatables.net-responsive-bs5';
 import Select from 'datatables.net-select-bs5';
 
+import 'datatables.net-buttons/js/buttons.html5';
+import 'datatables.net-buttons/js/buttons.print';
+
 import 'datatables.net-bs5/css/dataTables.bootstrap5.min.css';
 import 'datatables.net-select-bs5/css/select.bootstrap5.min.css';
+import 'datatables.net-select-bs5/css/select.bootstrap5.min.css';
+import 'datatables.net-responsive-bs5/css/responsive.bootstrap5.min.css';
+import 'datatables.net-fixedheader-bs5/css/fixedHeader.bootstrap5.min.css';
+import 'datatables.net-buttons-bs5/css/buttons.bootstrap5.min.css';
 import Swal from 'sweetalert2';
 
 DataTable.use(Select);
@@ -17,6 +24,9 @@ DataTable.use(Buttons);
 DataTable.use(FixedColumns);
 DataTable.use(FixedHeader);
 DataTable.use(Responsive);
+
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+window.JSZip = JSZip;
 
 const offcanvasEl = document.getElementById('userOffcanvas');
 const offcanvas = bootstrap.Offcanvas.getOrCreateInstance(offcanvasEl);
@@ -30,6 +40,7 @@ $.ajaxSetup({
 const userTable = new DataTable('#userTbl', {
     processing: true,
     serverSide: true,
+    dom: 'Bfrtip',
     ajax: {
         url: '/users/ajaxloadusers',
         type: 'POST',
