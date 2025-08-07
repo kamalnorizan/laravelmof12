b71901f7386da397f44ec3ae204fd6ac7dd8d2a1
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2025-08-07 11:59:58 +0800

    sanctum

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        $token = $user->createToken('API Token')->plainTextToken;
+
+        return response()->json(['success' => true, 'message' => 'Login successful', 'token' => $token]);
+    }
+}
