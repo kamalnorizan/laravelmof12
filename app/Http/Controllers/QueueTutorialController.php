c3819809c8aeee74bd9532af0973165dfd83f5f5
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2025-08-08 11:13:45 +0800

    update

--- app/Http/Controllers/QueueTutorialController.php
+++ app/Http/Controllers/QueueTutorialController.php
@@ -8,11 +8,11 @@ use Illuminate\Support\Facades\Mail;
 class QueueTutorialController extends Controller
 {
     public function testmail() {
-        // Mail::to('test@gmail.com')->send(new \App\Mail\PaymentSuccessfulMail());
+        Mail::to('test@gmail.com')->send(new \App\Mail\PaymentSuccessfulMail());
 
-        for($i = 0; $i < 100; $i++) {
-            \App\Jobs\StuckJob::dispatch();
-        }
+        // for($i = 0; $i < 100; $i++) {
+        //     \App\Jobs\StuckJob::dispatch();
+        // }
 
         return redirect()->route('invoices.index')->with('success', 'Test email sent successfully!');
     }
